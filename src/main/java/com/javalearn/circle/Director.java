18960cae2f40d0ba8d38c32ac969be309100c5ca
VladAcronic <drichkovvlad@gmail.com>
2022-02-14 04:50:29 +0700

    version 1.1.03 after first Filipp's corrections without Logging

--- src/main/java/com/javalearn/circle/Director.java
+++ src/main/java/com/javalearn/circle/Director.java
@@ -1,7 +1,6 @@
 package com.javalearn.circle;
 
 import com.javalearn.circle.data.DataReader;
-import com.javalearn.circle.data.SimpleDataReader;
 import com.javalearn.circle.entity.Circle;
 import com.javalearn.circle.exceptions.DataException;
 import com.javalearn.circle.logic.CircleCreator;
@@ -12,28 +11,19 @@ import java.util.List;
 
 public class Director {
 
-    private DataReader reader;// = new SimpleDataReader();
-    private CircleValidator validator;// = new CircleValidator();
-    private CircleCreator creator;// = new CircleCreator();
+    private final DataReader reader;
+    private final CircleValidator validator;
+    private final CircleCreator creator;
 
 
-    public Director (DataReader reader, CircleValidator validator, CircleCreator creator){
+    public Director(DataReader reader, CircleValidator validator, CircleCreator creator) {
         this.reader = reader;
         this.validator = validator;
         this.creator = creator;
     }
 
-/*
-    Director (){
-        this.reader = new SimpleDataReader();
-        this.validator = new CircleValidator();
-        this.creator = new CircleCreator();
-    }
 
- */
-
-
-    public List<Circle> readAndCreateCircles (String path) throws DataException {
+    public List<Circle> readAndCreateCircles(String path) throws DataException {
         List<Circle> circles = new ArrayList<>();
         List<String> lines = reader.read(path);
 
